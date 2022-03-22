0f5ea274442315d8b962c1d3d25c8209df34c505
insung93 <jis5780@gmail.com>
2022-03-22 23:57:54 +0900

    use class

---
+++ src/012/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ClassPrototype from './R012_Class&Prototype';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>012 - class 사용하기</p>
+            <ClassPrototype />
+        </div>
+    );
+}
+
