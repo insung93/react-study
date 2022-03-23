c9296d48a3ae132b92c8ddd2f04955e20735d1a9
insung93 <jis5780@gmail.com>
2022-03-23 21:35:30 +0900

    013 Arrow Function

---
+++ src/013/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ArrowFunc from './R013_ArrowFunction';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>013 - 화살표 함수 사용하기</p>
+            <ArrowFunc />
+        </div>
+    );
+}
+
