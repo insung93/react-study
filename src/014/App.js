65784d0c52c11e24c94293706c03366f0e1caadb
insung93 <jis5780@gmail.com>
2022-03-25 23:56:14 +0900

    015 map() function

---
+++ src/014/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ForEach from './R014_ForEach';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>014 - forEach() 함수 사용하기</p>
+            <ForEach />
+        </div>
+    );
+}
+
---
+++ src/015/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Map from './R015_Map';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>015 - map() 함수 사용하기</p>
+            <Map />
+        </div>
+    );
+}
+
