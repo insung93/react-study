36eea6f4e4f2f2b8b04702bff5465d50ca1c8bed
insung93 <jis5780@gmail.com>
2022-03-28 18:52:08 +0900

    017 use props

---
+++ src/017/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Props from './R017_Props';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>017 - props 사용하기</p>
+            <Props props_val="THIS IS PROPS" />
+        </div>
+    );
+}
+
