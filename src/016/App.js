b2ae7a584d10f2ff443dff571430a273089d5b97
insung93 <jis5780@gmail.com>
2022-03-28 18:45:54 +0900

    use jQuery

---
+++ src/016/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Jquery from './R016_Jquery';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>016 - jQuery 사용하기</p>
+            <Jquery />
+        </div>
+    );
+}
+
