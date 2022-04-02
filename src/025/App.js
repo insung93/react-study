945ae912fd818181a06ebd57bcbe81944ef60a22
insung93 <jis5780@gmail.com>
2022-04-03 07:28:01 +0900

    025 setState 사용하기

---
+++ src/025/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SetStete from './R025_setState';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>025 - setState 사용하기</p>
+            <SetStete />
+        </div>
+    );
+}
