fcfa2deba0528b228b0918e70a62eae223b01cd8
insung93 <jis5780@gmail.com>
2022-04-02 21:41:47 +0900

    state 사용하기

---
+++ src/024/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ReactState from './R024_ReactState';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>024 - state 사용하기</p>
+            <ReactState reactString={"react"} />
+        </div>
+    );
+}
