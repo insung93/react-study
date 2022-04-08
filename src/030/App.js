f32bcade3443508832ed891faabfac953e1cd066
insung93 <jis5780@gmail.com>
2022-04-08 23:45:31 +0900

    030 - 함수형 컴포넌트

---
+++ src/030/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import FunctionComponent from './R030_FunctionComponent';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>030 - 함수형 컴포넌트 사용하기</p>
+            <FunctionComponent contents="[THIS IS FunctionComponent]" />
+        </div>
+    );
+}
