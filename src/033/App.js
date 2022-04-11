974de7cb19ea758101301663b597f21c74a98ce1
insung93 <jis5780@gmail.com>
2022-04-11 23:49:27 +0900

    033 map()으로 element 반환하기

---
+++ src/033/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ReturnMap from './R033_ReturnMap';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>033 - map()으로 element 반환하기</p>
+            <ReturnMap />
+        </div>
+    );
+}
