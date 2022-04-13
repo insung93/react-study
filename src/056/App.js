81844f1ce20e7fc4b0bbe57fcbb31e7eb3aa77fa
insung93 <jis5780@gmail.com>
2022-04-13 19:04:20 +0900

    056,057 sweetalert2 사용

---
+++ src/056/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sweetalert2Basic from './R056_Sweetalert2Basic';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>056 - sweetalert2 Basic 사용하기</p>
+            <Sweetalert2Basic />
+        </div>
+    );
+}
---
+++ src/057/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sweetalert2Position from './R057_Sweetalert2Position';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>057 - sweetalert2 position 사용하기</p>
+            <Sweetalert2Position />
+        </div>
+    );
+}
