3548ab363c872dff8c2728d6423453c2de8c3aa1
insung93 <jis5780@gmail.com>
2022-04-14 21:27:13 +0900

    058 sweetalert2 confirm 사용하기

---
+++ src/058/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sweetalert2Confirm from './R058_Sweetalert2Confirm';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>058 - sweetalert2 confirm 사용하기</p>
+            <Sweetalert2Confirm />
+        </div>
+    );
+}
