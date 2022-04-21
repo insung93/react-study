f7e2ab31234eab42a02cdb6f4079d4dabcbad362
insung93 <jis5780@gmail.com>
2022-04-21 20:19:19 +0900

    065 Promise catch 사용

---
+++ src/065/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Promise from './R065_Promise';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>065 - Promise catch 사용하기</p>
+            <Promise />
+        </div>
+    );
+}
