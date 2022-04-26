1e3fdfd59208eb4a95d65f170f8d2ecafb5bc6a8
insung93 <jis5780@gmail.com>
2022-04-26 13:40:55 +0900

    076 컨텍스트 api 사용하기

---
+++ src/076/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContextApi from './Context/R076_ContextApi';
+
+export default function () {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>076 - 컨텍스트 api 사용하기</p>
+            <ContextApi />
+        </div>
+    );
+}
