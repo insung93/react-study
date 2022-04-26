e35a7a566fc40c3d6198168f0e047765ed7149e8
insung93 <jis5780@gmail.com>
2022-04-27 08:41:19 +0900

    077 - 컨텍스트로 부모 데이터 변경

---
+++ src/077/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ContextApi from './Context/R077_ContextApi';
+
+function App() {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>077 - 컨텍스트로 부모데이터 변경하기</p>
+            <ContextApi />
+        </div>
+    );
+}
+
+export default App;
