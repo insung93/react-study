46ba34495117e80385bd78f479a3b74ffe248c7f
insung93 <jis5780@gmail.com>
2022-04-28 20:50:21 +0900

    078~080 redux 사용

---
+++ src/078/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+const store = createStore(reducers);
+
+const listener = () => {
+    ReactDOM.render(
+        <App store={store} />,
+        document.getElementById('root')
+    );
+};
+store.subscribe(listener);
+listener();
